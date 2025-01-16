adb74966f8ddb5f1f8f548d0c8f860246c303f5d
Olivia Pateman <olivia.pateman@ten10.com>
2025-01-16 14:33:56 +0000

    Spring reads importIMBD class
    Method is called in the CommandLine.
    Removed logger.

--- src/main/java/com/example/IMDbExercise/ImDbExerciseApplication.java
+++ src/main/java/com/example/IMDbExercise/ImDbExerciseApplication.java
@@ -7,24 +7,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class ImDbExerciseApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(ImDbExerciseApplication.class);
-
     public static void main(String[] args) {
-		SpringApplication.run(ImDbExerciseApplication.class, args);
+		SpringApplication.run(ImDbExerciseApplication.class,args);
 	}
 
 	@Bean
-	public CommandLineRunner showData(MoviesRepository repository){
+	public CommandLineRunner showData(ImportIMDBData importIMDBData){
 		return (args) -> {
-			log.info("Movies found by searching for title: ");
-			log.info("-------------------------------");
-			repository.findByPrimaryTitle("the").forEach(movies -> {
-				log.info(movies.toString());
-			});
-			log.info("");
+			importIMDBData.importFile();
 		};
 	}
 
