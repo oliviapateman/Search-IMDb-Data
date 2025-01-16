951c0daa3f4ccaa7bb016ca70aa87589953c75f1
Olivia Pateman <olivia.pateman@ten10.com>
2025-01-16 16:27:33 +0000

    Removed unnecessary imports.

--- src/main/java/com/example/IMDbExercise/ImDbExerciseApplication.java
+++ src/main/java/com/example/IMDbExercise/ImDbExerciseApplication.java
@@ -1,13 +1,9 @@
 package com.example.IMDbExercise;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class ImDbExerciseApplication {
