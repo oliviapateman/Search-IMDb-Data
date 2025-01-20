325d21eee1f05f5f5f49a7150ac9a4128cdf1429
Olivia Pateman <olivia.pateman@ten10.com>
2025-01-20 16:20:17 +0000

    Updated ExerciseApp to run the basics methods.
    Renamed DataHeaders to BasicsDataHeaders.
    Renamed ImportIMDBData to ImportBasics.

--- src/main/java/com/example/IMDbExercise/DataHeaders.java
+++
@@ -1,5 +0,0 @@
-package com.example.IMDbExercise;
-
-public enum DataHeaders {
-    tconst, titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runtimeMinutes, genres
-}
--- src/main/java/com/example/IMDbExercise/ImDbExerciseApplication.java
+++ src/main/java/com/example/IMDbExercise/ImDbExerciseApplication.java
@@ -1,9 +1,14 @@
 package com.example.IMDbExercise;
 
+import org.apache.commons.csv.CSVParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
 public class ImDbExerciseApplication {
@@ -13,9 +18,11 @@ public class ImDbExerciseApplication {
 	}
 
 	@Bean
-	public CommandLineRunner showData(ImportIMDBData importIMDBData){
+	public CommandLineRunner showData(ImportBasics basics){
 		return (args) -> {
-			importIMDBData.importFile();
+			Path path = basics.getBasicsFileFromDir();
+			CSVParser parser = basics.importFile(path);
+			basics.basicsParsing(parser);
 		};
 	}
 
